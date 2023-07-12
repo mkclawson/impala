a4190732f4fea1fa86d603987f06f4c4fd09b4cc
Meet Kumar Jain <meetkjain83@gmail.com>
2023-07-12 18:01:39 +0530

    AutoBlocking enablement based on conditions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,6 @@
 import {
   sampleRUM,
-  // buildBlock,
+  buildBlock,
   loadHeader,
   loadFooter,
   decorateButtons,
@@ -19,32 +19,35 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
  * Builds hero block and prepends to main in a new section.
  * @param {Element} main The container element
  */
-/*
+
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
   const picture = main.querySelector('picture');
+  console.log(picture.closest('h1'));
   // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
+  if (h1 && picture && (!h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
     section.append(buildBlock('hero', { elems: [picture, h1] }));
     main.prepend(section);
   }
 }
-*/
+
 /**
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
  */
-/*
+
 function buildAutoBlocks(main) {
- try {
-   buildHeroBlock(main);
- } catch (error) {
-   // eslint-disable-next-line no-console
-   console.error('Auto Blocking failed', error);
- }
+  try {
+    const heroelement = main.querySelector('.hero-container');
+    const h1element = heroelement.nextElementSibling.querySelector('h1');
+    if (!h1element) buildHeroBlock(main);
+  } catch (error) {
+    // eslint - disable - next - line no - console
+    console.error('Auto Blocking failed', error);
+  }
 }
-*/
+
 /**
  * Decorates the main element.
  * @param {Element} main The main element
@@ -54,7 +57,7 @@ export function decorateMain(main) {
   // hopefully forward compatible button decoration
   decorateButtons(main);
   decorateIcons(main);
-  // buildAutoBlocks(main);
+  buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
 }
