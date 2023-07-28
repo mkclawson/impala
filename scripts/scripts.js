194b74e76402686102ad16ad6a0f4ff6f2f408cd
Meet Kumar Jain <meetkjain83@gmail.com>
2023-07-28 19:55:43 +0530

    Fixing the lint issues

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -40,7 +40,7 @@ function buildAutoBlocks(main) {
   try {
     buildHeroBlock(main);
   } catch (error) {
-    // eslint - disable - next - line no - console
+    // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
   }
 }
