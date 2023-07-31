5b0446c36b3a9f86ce810ce1deeff00234c9ddbf
Meet Kumar Jain <meetkjain83@gmail.com>
2023-07-31 16:30:03 +0530

    Fixing the JS Code for Updated parameter

--- blocks/blogintro/blogintro.js
+++ blocks/blogintro/blogintro.js
@@ -7,7 +7,7 @@ export default function decorate(block) {
     }
     if (r === 2) {
       const attr = [...row.children][0].textContent.toLowerCase().includes(':') ? [...row.children][0].textContent.toLowerCase().slice(0, -1) : [...row.children][0].textContent.toLowerCase();
-      if (document.querySelector(`meta[name='${attr}']`).content) {
+      if (document.querySelector(`meta[name='${attr}']`)) {
         [...row.children][1].textContent = document.querySelector(`meta[name='${attr}']`).content;
       } else { row.remove(); }
     }
