bfa5407b05c40ecc61498987e952a227ae68f216
Meet Kumar Jain <meetkjain83@gmail.com>
2023-08-16 18:44:46 +0530

    Adding CSS code for recent posts

--- blocks/recent-posts/recent-posts.js
+++ blocks/recent-posts/recent-posts.js
@@ -7,25 +7,11 @@ function createCard(row, style) {
   if (row.image && row.title) card.prepend(createOptimizedPicture(row.image));
   const cardContent = document.createElement('div');
   cardContent.classList.add('card-content');
-  const author = document.createElement('div');
-  author.classList.add('blog-author');
-  if (row.author && row.author !== '0') author.innerHTML += `${row.author}`;
-  const publishnread = document.createElement('div');
-  publishnread.classList.add('blog-publishnread');
-  const publishdiv = document.createElement('div');
-  const readtimediv = document.createElement('div');
-  if (row.published && row.published !== '0') publishdiv.innerHTML += `${row.published}`;
-  if (row.readtime && row.readtime !== '0') readtimediv.innerHTML += ` ${row.readtime}`;
-  publishnread.append(publishdiv);
-  publishnread.append(readtimediv);
-  cardContent.append(author);
-  cardContent.append(publishnread);
   const link = document.createElement('a');
   link.classList.add('blog-link');
   link.href = row.path;
   if (row.title) link.innerHTML = `<h5>${row.title}</h5>`;
   cardContent.append(link);
-  if (row.description) link.innerHTML += `<p>${row.description}</p>`;
   card.append(cardContent);
   return (card);
 }
