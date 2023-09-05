725065085db1066d237d87becb7945b3e051216d
Dave Fink <138233107+dave-fink@users.noreply.github.com>
2023-09-05 15:12:00 -0400

    Issue #7 added Disqus commenting (#34)
    * Issue #7 added Disqus commenting
    
    * Issue #7 added Disqus commenting - moved to delayed.js
    
    * Issue #7 added Disqus commenting - moved to delayed.js
    
    * Issue #7 fixing eslint errors

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,21 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
+import { div } from './dom-helpers.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+// disgus commenting
+function disqus() {
+  const script = document.createElement('script');
+  script.src = 'https://https-main-impala-mkclawson-hlx-live.disqus.com/embed.js';
+  script.setAttribute('data-timestamp', +new Date());
+  (document.head || document.body).appendChild(script);
+
+  // insert disqus thread after loves
+  const loves = document.querySelector('.loves-wrapper');
+  loves.insertAdjacentElement('afterend', div({ id: 'disqus_thread' }));
+}
+disqus();
