176306e952d3e9837a1a0a410873d8fe669a0a1c
dave-fink <david@keypixel.com>
2023-09-05 12:40:11 -0400

    Issue #7 added Disqus commenting

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -6,23 +6,28 @@ import { div } from './dom-helpers.js';
 sampleRUM('cwv');
 
 // disgus commenting
-const script = document.createElement('script');
-script.src = 'https://https-main-impala-mkclawson-hlx-live.disqus.com/embed.js';
-script.setAttribute('data-timestamp', +new Date());
-(document.head || document.body).appendChild(script);
+function disqus() {
+  // inject discuss script
+  const script = document.createElement('script');
+  script.src = 'https://https-main-impala-mkclawson-hlx-live.disqus.com/embed.js';
+  script.setAttribute('data-timestamp', +new Date());
+  (document.head || document.body).appendChild(script);
 
-// Create details element
-const details = document.createElement('details');
-details.classList.add('disqus');
+  // Create details element
+  const details = document.createElement('details');
+  details.classList.add('disqus');
 
-// Create summary element
-const summary = document.createElement('summary');
-summary.textContent = 'Comments';
+  // Create summary element
+  const summary = document.createElement('summary');
+  summary.textContent = 'Comments';
 
-// Append the summary and content to the details element
-details.appendChild(summary);
-details.appendChild(div({ id: 'disqus_thread' }));
+  // Append the summary and content to the details element
+  details.appendChild(summary);
+  details.appendChild(div({ id: 'disqus_thread' }));
 
-// insert comments after loves
-const loves = document.querySelector('.blog-cards-parent');
-loves.insertAdjacentElement('afterend', details);
+  // insert comments after loves
+  const loves = document.querySelector('.blog-cards-parent');
+  loves.insertAdjacentElement('afterend', details);
+}
+
+disqus();
