a08f08b048c9fd5fd8c12cb99140ab247f21242d
dave-fink <david@keypixel.com>
2023-09-05 12:40:11 -0400

    Issue #7 added Disqus commenting - moved to delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,21 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+// disgus commenting configuration
+import { div } from '../../scripts/dom-helpers.js';
+
+var disqus_config = function () {
+this.page.url = window.location.href;
+};
+(function() {
+var d = document, s = d.createElement('script');
+s.src = 'https://https-main-impala-mkclawson-hlx-live.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+
+// insert disqus thread
+const loves = document.querySelector('.loves-wrapper');
+const disqus = div({ id: 'disqus_thread' });
+loves.insertAdjacentElement('afterend', disqus);
