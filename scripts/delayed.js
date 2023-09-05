0d9bb65db95d587823c833ecd8dc8dad793189ef
dave-fink <david@keypixel.com>
2023-09-05 12:40:11 -0400

    Issue #7 fixing eslint errors

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,25 +1,21 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
+import { div } from './dom-helpers.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
 
-// disgus commenting configuration
-import { div } from '../../scripts/dom-helpers.js';
+// disgus commenting
+function disqus() {
+  const script = document.createElement('script');
+  script.src = 'https://https-main-impala-mkclawson-hlx-live.disqus.com/embed.js';
+  script.setAttribute('data-timestamp', +new Date());
+  (document.head || document.body).appendChild(script);
 
-var disqus_config = function () {
-this.page.url = window.location.href;
-};
-(function() {
-var d = document, s = d.createElement('script');
-s.src = 'https://https-main-impala-mkclawson-hlx-live.disqus.com/embed.js';
-s.setAttribute('data-timestamp', +new Date());
-(d.head || d.body).appendChild(s);
-})();
-
-// insert disqus thread
-const loves = document.querySelector('.loves-wrapper');
-const disqus = div({ id: 'disqus_thread' });
-loves.insertAdjacentElement('afterend', disqus);
+  // insert disqus thread after loves
+  const loves = document.querySelector('.loves-wrapper');
+  loves.insertAdjacentElement('afterend', div({ id: 'disqus_thread' }));
+}
+disqus();
