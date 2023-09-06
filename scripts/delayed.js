ee46bebf2ad844d2fb9e546ebf086e516e808adc
Dave Fink <138233107+dave-fink@users.noreply.github.com>
2023-09-06 14:34:37 -0400

    Issue #35: Adding disqus comments (#38)

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,17 +5,28 @@ import { div } from './dom-helpers.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
-// add more delayed functionality here
-
-// disgus commenting
 function disqus() {
+  // inject discuss script
   const script = document.createElement('script');
   script.src = 'https://https-main-impala-mkclawson-hlx-live.disqus.com/embed.js';
   script.setAttribute('data-timestamp', +new Date());
   (document.head || document.body).appendChild(script);
 
-  // insert disqus thread after loves
-  const loves = document.querySelector('.loves-wrapper');
-  loves.insertAdjacentElement('afterend', div({ id: 'disqus_thread' }));
+  // Create details element
+  const details = document.createElement('details');
+  details.classList.add('disqus');
+
+  // Create summary element
+  const summary = document.createElement('summary');
+  summary.textContent = 'Comments';
+
+  // Append the summary and content to the details element
+  details.appendChild(summary);
+  details.appendChild(div({ id: 'disqus_thread' }));
+
+  // insert comments after loves
+  const loves = document.querySelector('.blog-cards-parent');
+  loves.insertAdjacentElement('afterend', details);
 }
+
 disqus();
