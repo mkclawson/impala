20324da8cce3ed5aa67132ef30f703caa6e43d53
jzachari <jzachari@adobe.com>
2023-10-17 16:29:37 -0700

    .

--- blocks/parallax/parallax.js
+++ blocks/parallax/parallax.js
@@ -1,21 +1,14 @@
-function parallax(block, multiplier = 0.4) {
-  window.addEventListener('scroll', () => {
-    const offset = window.scrollY;
-    /* The number of pixels subtracted here should match the negative
-     * number of pixels in the CSS line 13 to avoid a jump upon scrolling */
-    block.style.backgroundPositionY = `${offset * multiplier - 150}px`;
-  });
-}
-
 export default async function decorate(block) {
   // Select the parallax-wrapper div
-  //const parallaxDiv = document.querySelector('.parallax-wrapper');
+  // const parallaxDiv = document.querySelector('.parallax-wrapper');
   // Write the parallax-wrapper div to the console
   // console.log(parallaxDiv);
   // Select the picture element in parallaDiv
-  //const picture = parallaxDiv.querySelector('picture');
+  // const picture = parallaxDiv.querySelector('picture');
   // Wtite picture to the console
   // console.log(picture);
+  // remove the parent element of picture
+  // picture.parentElement.remove();
   // Select the img element in picture
   // const img = picture.querySelector('img');
   // Write img to the console
@@ -30,27 +23,17 @@ export default async function decorate(block) {
   //parallaxDiv.appendChild(parallaxBackground);
   // Set the background image of the body to the imgSource
   //parallaxBackground.style.backgroundImage = `url(${imgSource})`;
+  // Select the picture element
+  const picture = block.querySelector('picture');
+  // Hide the picture element
+  picture.style.display = 'none';
   const columns = [...block.children];
   const images = columns[0].querySelectorAll(':scope > div');
   const [desktopImage] = images[0].querySelector('img').src.split('?');
-  let multiplier = 0.4;
-  // TODO: finish mobile image or delete
-  let mobileImage;
-  if (images.length > 1) {
-    [mobileImage] = images[1].querySelector('img').src.split('?');
-  }
-
   const parallaxBlock = document.createElement('div');
   parallaxBlock.classList.add('parallax-background');
   parallaxBlock.append(columns[0].querySelector(':scope > div'));
-
   parallaxBlock.style.backgroundImage = `url(${desktopImage})`;
-  if (window.innerWidth < 900) {
-    parallaxBlock.style.backgroundImage = `url(${mobileImage})`;
-    multiplier = 0.2;
-  }
-
-  block.textContent = '';
+  // document.style.backgroundImage = `url(${desktopImage})`;
   block.append(parallaxBlock);
-  parallax(parallaxBlock, multiplier);
 }
