cf8ccb69f396e7b8a528e8e8df30e689378bca85
jzachari <jzachari@adobe.com>
2023-10-25 18:57:36 -0700

    .

--- blocks/parallax/parallax.js
+++ blocks/parallax/parallax.js
@@ -3,13 +3,20 @@ export default async function decorate(block) {
   const picture = block.querySelector('picture');
   // Hide the picture element
   picture.style.display = 'none';
+  // Get the image source of picture element
+  const mobileImage = picture.querySelector('img').src;
+  // Write mobileImage to the console
+  // console.log(mobileImage);
   const columns = [...block.children];
   const images = columns[0].querySelectorAll(':scope > div');
   const [desktopImage] = images[0].querySelector('img').src.split('?');
+  // Write desktopImage to the console
+  // console.log(desktopImage);
   const parallaxBlock = document.createElement('div');
   parallaxBlock.classList.add('parallax-background');
-  // parallaxBlock.append(columns[0].querySelector(':scope > div'));
   parallaxBlock.style.backgroundImage = `url(${desktopImage})`;
-  // document.style.backgroundImage = `url(${desktopImage})`;
+  if (window.innerWidth < 900) {
+    parallaxBlock.style.backgroundImage = `url(${mobileImage})`;
+  }
   block.append(parallaxBlock);
 }
